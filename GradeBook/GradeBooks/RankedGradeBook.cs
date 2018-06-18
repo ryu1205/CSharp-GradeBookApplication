93b038c7cd06e3316b0e47fb6fecca5cc5a7dd9a
ryu1205 <ryu_1205@hotmail.com>
2018-06-18 15:10:29 +0000

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    // StandardGradeBook CLASS inherits BaseGradeBook CLASS
+    public class RankedGradeBook : BaseGradeBook
+    {
+        // Constructor: it calls the base constructor(BaseGradeBook class's constructor) upon completion
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    // StandardGradeBook CLASS inherits BaseGradeBook CLASS
+    public class StandardGradeBook : BaseGradeBook
+    {
+        // Constructor: it calls the base constructor(BaseGradeBook class's constructor) upon completion
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
