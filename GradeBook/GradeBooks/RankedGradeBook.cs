95f29b27a362181553683ff64751a7cc9395c58e
ryu1205 <ryu_1205@hotmail.com>
2018-06-19 09:36:34 +0000

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,11 +34,30 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
 
-            /*for (int i=0; grades.Count;i++)
+        //
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
             {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                return;
+            }             
 
-            }*/
+            base.CalculateStatistics();
+        }
+
+        //
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
