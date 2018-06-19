8664cfe8b03505a651eee478b7ee64402221f2be
ryu1205 <ryu_1205@hotmail.com>
2018-06-19 09:16:09 +0000

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,11 +26,11 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList() ;
             if (grades[thresold-1] <= averageGrade)
                 return 'A';
-            else if ((grades[thresold * 2] - 1) <= averageGrade)
+            else if (grades[thresold * 2] - 1 <= averageGrade)
                 return 'B';
-            else if ((grades[thresold * 3] - 1) <= averageGrade)
+            else if (grades[thresold * 3] - 1 <= averageGrade)
                 return 'C';
-            else if ((grades[thresold * 4] - 1) <= averageGrade)
+            else if (grades[thresold * 4] - 1 <= averageGrade)
                 return 'D';
             else return 'F';
 
