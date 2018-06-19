08d34830f031fd119a2e5c11110aa27b817f5427
ryu1205 <ryu_1205@hotmail.com>
2018-06-19 09:17:38 +0000

    a

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grade requires at least 5 students");
+                throw new InvalidOperationException("(Ranked-grading requires a minimum of 5 students to work");
 
             var thresold = (int)Math.Ceiling(Students.Count*0.2);
 
